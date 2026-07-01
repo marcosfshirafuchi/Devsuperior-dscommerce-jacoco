e59771c085b8c5ca88faf7667bc1208a1c0a48ba
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2026-07-01 08:44:06 -0300

    Teste carregando usuario

---
+++ dscommerce-backend-aulas-cap05-3.4.4/src/test/java/com/devsuperior/dscommerce/services/UserServiceTests.java
@@ -0,0 +1,68 @@
+package com.devsuperior.dscommerce.services;
+
+import com.devsuperior.dscommerce.entities.User;
+import com.devsuperior.dscommerce.projections.UserDetailsProjection;
+import com.devsuperior.dscommerce.repositories.UserRepository;
+import com.devsuperior.dscommerce.tests.UserDetailsFactory;
+import com.devsuperior.dscommerce.tests.UserFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ExtendWith(SpringExtension.class)
+public class UserServiceTests {
+
+    @InjectMocks
+    private UserService service;
+
+    @Mock
+    private UserRepository repository;
+
+    private String existingUsername, nonExistingUsername;
+    private User user;
+    private List<UserDetailsProjection> userDetails;
+
+
+    @BeforeEach
+    void setUp() throws Exception {
+
+        existingUsername = "maria@gmail.com";
+        nonExistingUsername = "user@gmail.com";
+        user = UserFactory.createCustomClientUser(1L, existingUsername);
+        userDetails = UserDetailsFactory.createCustomAdminUser(existingUsername);
+
+        /*Problema 5: Carregar usuário
+
+        Implemente os testes unitários do método UserService.loadUserByUsername, cobrindo todos os cenários importantes. Você deve identificar os casos de testes necessários para cobertura completa do método loadUserByUsername.
+
+        * */
+        Mockito.when(repository.searchUserAndRolesByEmail(existingUsername)).thenReturn(userDetails);
+        Mockito.when(repository.searchUserAndRolesByEmail(nonExistingUsername)).thenReturn(new ArrayList<>());
+    }
+
+    @Test
+    public void loadUserByUserNameShouldReturnUserDetailsWhenUserExists() {
+        UserDetails result = service.loadUserByUsername(existingUsername);
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(result.getUsername(), existingUsername);
+    }
+
+    @Test
+    public void loadUserByUserNameShouldThrowUsernameNotFoundExceptionWhenUserDoesNotExists() {
+        Assertions.assertThrows(UsernameNotFoundException.class, () -> {
+            service.loadUserByUsername(nonExistingUsername);
+        });
+    }
+
+
+}
